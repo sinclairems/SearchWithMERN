3a81fb73d5d006dc5f591d16bba04db906f10faf
Erin <50000677+sinclairems@users.noreply.github.com>
2024-05-23 14:39:43 -0500

    chore: Update Apollo Client configuration and add GraphQL queries and mutations

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,19 @@
-import './App.css';
-import { Outlet } from 'react-router-dom';
+import "./App.css";
+import { Outlet } from "react-router-dom";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
-import Navbar from './components/Navbar';
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+      <div>
+        <Outlet />
+      </div>
+    </ApolloProvider>
   );
 }
 
